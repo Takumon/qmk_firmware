7be8b02300e8721657b1f106b90995b661fd7562
Takumon <inouetakumon@gmail.com>
2023-02-07 01:33:40 +0900

    日本語対応

--- keyboards/crkbd/keymaps/Takumon/keymap.c
+++ keyboards/crkbd/keymaps/Takumon/keymap.c
@@ -19,61 +19,108 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 
+#define JP_MHEN KC_INT5 // Muhenkan (無変換)
+#define JP_HENK KC_INT4 // Henkan (変換)
+
+#define JP_DQUO S(KC_2) // "
+
+#define JP_AMPR S(KC_6) // &
+#define JP_QUOT S(KC_7) // '
+#define JP_GRV S(KC_LBRC) // `
+#define JP_EQL S(KC_MINS) // =
+#define JP_COLN KC_QUOT // :
+
+#define JP_BSLS KC_INT1 // (backslash)
+#define JP_PLUS S(KC_SCLN) // +
+#define JP_ASTR S(KC_QUOT) // *
+#define JP_PIPE S(KC_INT3) // |
+
+#define JP_AT KC_LBRC // @
+#define JP_CIRC KC_EQL // ^
+#define JP_TILD S(KC_EQL) // ~
+#define JP_UNDS S(KC_INT1) // _
+
+#define JP_LPRN S(KC_8) // (
+#define JP_RPRN S(KC_9) // )
+#define JP_LBRC KC_RBRC // [
+#define JP_RBRC KC_NUHS // ]
+#define JP_LCBR S(KC_RBRC) // {
+#define JP_RCBR S(KC_NUHS) // }
+
+#define WIN1 LGUI(KC_1)
+#define WIN2 LGUI(KC_2)
+#define WIN3 LGUI(KC_3)
+#define WIN4 LGUI(KC_4)
+#define WIN_LEFT LGUI(LCTL(KC_LEFT))
+#define WIN_RGHT LGUI(LCTL(KC_RIGHT))
+#define WIN_SCRNSHT LGUI(LSFT(KC_S))
 
 enum custom_keycodes {
-    UP5TIMES = SAFE_RANGE,
-    DOWN5TIMES,
+    UP5 = SAFE_RANGE,
+    DOWN5,
+    BSPCDEL,
+};
+
+enum {
+  TD_HENKAN = 0,
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_HENKAN] = ACTION_TAP_DANCE_DOUBLE(JP_MHEN, JP_HENK),
 };
 
 
+// TODO 日本語の鉤括弧の入力
+// TODO alt tab入力を簡単に
+static uint8_t saved_mods = 0;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_LGUI,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_MINS,
+      KC_LWIN,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  BSPCDEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_ENT,
+       KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, LT(3,KC_ENT),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_AMPR,
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-              MT(MOD_LALT | MOD_RALT,KC_INT5), KC_LCTL, LT(2,KC_ENT),   MT(MOD_LSFT,KC_BSPC),  LT(1,KC_SPC) , KC_INT2
+                                     KC_LALT, KC_LCTL, LT(2,KC_SPC),  LT(2,KC_ESC), LT(1,KC_SPC), TD(TD_HENKAN)
                                       //`--------------------------'  `--------------------------'
 
   ),
 
   [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_LGUI,KC_EXLM,  KC_AT,  KC_HASH,  KC_DLR, KC_PERC,                      KC_HOME,UP5TIMES,   KC_UP,  KC_ESC,   KC_NO,  KC_NO,
+      KC_LWIN, KC_EXLM, JP_DQUO, KC_HASH,  KC_DLR, KC_PERC,                        JP_AT,     UP5,   KC_UP, JP_CIRC, JP_TILD, BSPCDEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TAB, KC_CIRC, KC_AMPR, KC_LCBR, KC_UNDS, KC_QUOT,                        KC_DEL, KC_LEFT, KC_DOWN, KC_RGHT,  KC_END, KC_ENT,
+       KC_TAB, JP_AMPR, JP_QUOT,  JP_GRV,  JP_EQL, JP_COLN,                      KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT,  KC_END, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_INT3, KC_COLN,KC_MINS,KC_DQUO,S(KC_INT3),                      KC_BSPC,  KC_F12,DOWN5TIMES,S(KC_INTERNATIONAL_1),  KC_F2, KC_F10,
+      KC_LSFT, JP_BSLS, JP_PLUS, KC_MINS, JP_ASTR, JP_PIPE,                      BSPCDEL,  KC_F12,   DOWN5,   KC_F2,  JP_UNDS, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-              MT(MOD_LALT | MOD_RALT,KC_INT5), KC_LCTL, KC_TRNS,   MT(MOD_LSFT,KC_BSPC),  KC_TRNS , KC_INT2
+                                          KC_LALT, KC_LCTL, KC_TRNS,    KC_TRNS, KC_TRNS, TD(TD_HENKAN)
                                       //`--------------------------'  `--------------------------'
   ),
 
   [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_LGUI,  KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,      KC_AUDIO_VOL_UP,KC_AUDIO_VOL_DOWN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      KC_LWIN,    KC_1,    KC_2,    KC_3,    KC_4,   KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0, BSPCDEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_TAB,   KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0,                      KC_ASTR, KC_LPRN, KC_RBRC, KC_NUHS, KC_NO, KC_ENT,
+       KC_TAB,    WIN1,    WIN2,    WIN3,    WIN4,  KC_ENT,                      JP_LPRN, JP_RPRN, JP_LBRC, JP_RBRC, XXXXXXX, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_DQUO, KC_COLN, KC_MINS,   KC_NO,   KC_NO,                      KC_RCBR,S(KC_NUHS), KC_LT,  KC_GT, XXXXXXX, XXXXXXX,
+      KC_LSFT, XXXXXXX, WIN_SCRNSHT,WIN_LEFT,WIN_RGHT,KC_F13,                    JP_LCBR, JP_RCBR,   KC_LT,   KC_GT, XXXXXXX, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-              MT(MOD_LALT | MOD_RALT,KC_INT5), KC_LCTL, KC_TRNS,   MT(MOD_LSFT,KC_BSPC),  KC_TRNS , KC_INT2
+                                          KC_LALT, KC_LCTL, KC_TRNS,    KC_TRNS, KC_TRNS, TD(TD_HENKAN)
                                       //`--------------------------'  `--------------------------'
   ),
 
   [3] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_LGUI,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      KC_LWIN, XXXXXXX,   KC_F2, XXXXXXX, XXXXXXX,   KC_F5,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_F10, BSPCDEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_ENT,
+       KC_TAB,  KC_F11,  KC_F12, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_VOLU, KC_VOLD, XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-              MT(MOD_LALT | MOD_RALT,KC_INT5), KC_LCTL, KC_TRNS,   MT(MOD_LSFT,KC_BSPC),  KC_TRNS , KC_INT2
+                                          KC_LALT, KC_LCTL, KC_TRNS,     KC_TRNS, KC_TRNS, TD(TD_HENKAN)
                                       //`--------------------------'  `--------------------------'
   )
 };
@@ -181,7 +228,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
 
   switch (keycode) {
-        case UP5TIMES:
+        case UP5:
             if (record->event.pressed) {
                 // five times
                 tap_code(KC_UP);
@@ -191,7 +238,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 tap_code(KC_UP);
             }
             return false;
-        case DOWN5TIMES:
+        case DOWN5:
             if (record->event.pressed) {
                 // five times
                 tap_code(KC_DOWN);
@@ -201,7 +248,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 tap_code(KC_DOWN);
             }
             return false;
-            break;
+        case BSPCDEL:
+            if (record->event.pressed) {
+                if (get_mods() & MOD_MASK_SHIFT) {
+                    saved_mods = get_mods() & MOD_MASK_SHIFT; // Mask off anything that isn't Shift
+                    del_mods(saved_mods); // Remove any Shifts present
+                    register_code(KC_DEL);
+                } else {
+                    saved_mods = 0; // Clear saved mods so the add_mods() below doesn't add Shifts back when it shouldn't
+                    register_code(KC_BSPC);
+                }
+            } else {
+                add_mods(saved_mods);
+                unregister_code(KC_DEL);
+                unregister_code(KC_BSPC);
+            }
+            return false;
     }
   return true;
 }
